8b82ad5d1fcb724cb88500e89fd439ea805eb920
Erick Araújo <araujoerick.dev@gmail.com>
2024-07-26 17:33:02 -0300

    :sparkles: feat: Lógica da página de Cadastro

--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -32,7 +32,7 @@ export const UserStorage = ({ children }) => {
       setLoading(true);
       const { url, options } = TOKEN_POST({ username, password });
       const tokenRes = await fetch(url, options);
-      if (!tokenRes.ok) throw new Error(`Error: ${tokenRes.statusText}`);
+      if (!tokenRes.ok) throw new Error("Error: Usuário ou Senha inválidos.");
 
       const { token } = await tokenRes.json();
       window.localStorage.setItem("token", token);
