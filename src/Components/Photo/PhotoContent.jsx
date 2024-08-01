85922ec63eb662215d45da1b261ae2c6f23833e1
Erick Araújo <araujoerick.dev@gmail.com>
2024-08-01 18:21:33 -0300

    :lipstick: feat: Estilização CSS do Feed Modal

--- src/Components/Photo/PhotoContent.jsx
+++ src/Components/Photo/PhotoContent.jsx
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 import PhotoComments from "./PhotoComments";
 
 const PhotoContent = ({ data }) => {
-  console.log(data);
   const { photo, comments } = data;
 
   return (
@@ -14,7 +13,7 @@ const PhotoContent = ({ data }) => {
       </div>
       <div className={styles.details}>
         <div>
-          <p>
+          <p className={styles.author}>
             <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
             <span className={styles.visualizacoes}>{photo.acessos}</span>
           </p>
