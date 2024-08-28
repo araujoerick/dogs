99f1ed3fd39906334d1746f243eaaee261f74060
Erick Araújo <araujoerick.dev@gmail.com>
2024-08-27 22:05:25 -0300

    :sparkles: feat: Lógica inicial da Página de Estatísticas

---
+++ src/Components/User/UserStatsGraphs.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./UserStatsGraphs.module.css";
+
+const UserStatsGraphs = ({ data }) => {
+  const [graph, setGraph] = React.useState([]);
+  const [total, setTotal] = React.useState(0);
+
+  return <div>UserStatsGraphs</div>;
+};
+
+export default UserStatsGraphs;
